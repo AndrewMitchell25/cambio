13c71424b0a41ecce7fb25125d7f279381a9b456
Andrew Mitchell <andrew.j.mitchell.247@gmail.com>
2024-11-10 17:03:19 -0500

    Game play mostly working

---
+++ cambio.py
@@ -0,0 +1,227 @@
+import random
+
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+        self.value = self.get_value()
+
+    def get_value(self):
+        card_nums = {'A': 1, 'J': 11, 'Q': 12, 'K': 13}
+        if self.rank not in card_nums:
+            return int(self.rank)
+        elif self.rank == 'K' and self.suit in 'HD':
+            return -1
+        else:
+            return card_nums[self.rank]
+
+    def name(self):
+        suit_names = {'C': "Clubs", 'H': "Hearts", 'S': "Spades", 'D': "Diamonds"}
+        rank_names = {'A': "Ace", 'J': "Jack", 'Q': "Queen", 'K': "King"}
+        rank = rank_names[self.rank] if self.rank in rank_names else self.rank
+        suit = suit_names[self.suit]
+        return rank + " of " + suit
+
+    def __str__(self):
+        return self.rank + self.suit
+
+class Deck():
+    def __init__(self):
+        self.suits = ['C', 'H', 'S', 'D']
+        self.ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        self.deck = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+        self.drawn = []
+        self.discarded = []
+
+        random.shuffle(self.deck)
+
+    def draw(self):
+        card = self.deck.pop() if self.deck else None #TODO: FIGURE OUT WHAT TO DO WHEN CARDS RUN OUT
+        self.drawn.append(card)
+        return card
+
+class Player():
+    def __init__(self):
+        self.cards = []
+        self.hand = None
+        self.score = 0
+
+    def __str__(self):
+        return " ".join(map(str, self.cards))
+
+
+class Cambio():
+    def __init__(self, num_players):
+        self.deck = None
+        self.played_cards = []
+        self.players = [Player() for _ in range(num_players)]
+        self.turn = 0
+        self.num_cards = 4
+        self.last_player = -1
+        self.last_turn = False
+        self.extra_cards = -1
+
+    def setup(self):
+        self.deck = Deck()
+        self.turn = 0
+        self.last_player = -1
+        self.last_turn = False
+
+        for i, player in enumerate(self.players):
+            player.hand = None
+            player.cards = []
+            for _ in range(self.num_cards):
+                player.cards.append(self.deck.draw())
+            if i == self.extra_cards:
+                player.cards.append(self.deck.draw())
+
+        self.extra_cards = -1
+
+
+    def game_state(self):
+        #TODO: UNFINISHED
+        game_state = {}
+        for i, player in enumerate(self.players):
+            game_state[f'Player{i}'] = str(player)
+        game_state['face-up'] = str(self.played_cards[-1]) if self.played_cards else "None"
+        return game_state
+
+    def draw(self):
+        self.players[self.turn].hand = self.deck.draw()
+
+    def play(self):
+        self.played_cards.append(self.players[self.turn].hand)
+        self.players[self.turn].hand = None
+
+    def place(self, pos):
+        if self.players[self.turn].cards[pos] == None:
+            raise ValueError(f"You do not have a card at position {pos}.")
+        temp = self.players[self.turn].cards[pos]
+        self.players[self.turn].cards[pos] = self.players[self.turn].hand
+        self.players[self.turn].hand = None
+        self.played_cards.append(temp)
+
+    def swap(self, pos1, player2, pos2):
+        if self.players[self.turn].cards[pos1] == None:
+            raise ValueError(f"You do not have a card at position {pos1}.")
+        if self.players[player2].cards[pos2] == None:
+            raise ValueError(f"Player {player2} does not have a card at position {pos2}.")
+        temp = self.players[self.turn].cards[pos1]
+        self.players[self.turn].cards[pos1] = self.players[player2].cards[pos2]
+        self.players[player2].cards[pos2] = temp
+
+    def stick(self, stick_player, stuck_player, pos):
+        if self.players[stuck_player].cards[pos] == None:
+            raise ValueError(f"Player {stuck_player} does not have a card at position {pos}.")
+        if self.players[stuck_player].cards[pos].rank != self.played_cards[-1].rank:
+            self.players[stick_player].cards.append(self.deck.draw())
+            raise ValueError(f"Player {stick_player} attempted to stick Player {stuck_player}'s card at position {pos} but failed.")
+        self.played_cards = self.players[stuck_player].cards[pos]
+        self.players[stuck_player].cards[pos] = None
+
+    def give(self, player1, pos1, player2, pos2):
+        if self.players[player1].cards[pos1] == None:
+            raise ValueError(f"")
+        if self.players[player2].cards[pos2] != None:
+            raise ValueError(f"")
+        self.players[player2].cards[pos2] = self.players[player1].cards[pos1]
+        self.players[player1].cards[pos1] = None
+
+    def has_power(self):
+        card = self.played_cards[-1]
+        if card.rank not in {'7', '8', '9', '10', 'J', 'Q', 'K'}:
+            return
+        elif card.rank in {'7', '8'}:
+            return "You may look at one of your cards. Enter the position of the card you'd like to look at. Enter -1 to decline the power."
+        elif card.rank in {'9', '10'}:
+            return "You may look at one of another player's cards. Enter the player number and the position of the card you'd like to look at. Enter -1 to decline the power."
+        elif card.rank in {'J', 'Q'}:
+            return "You may swap one of your cards with one of another player's cards. Enter the position of your card, followed by the player number and the position of the card you'd like to swap with. Enter -1 to decline the power."
+        elif card.rank == 'K':
+            return "You may look at another players card and decide if you want to swap with it or not. Enter the player number and the position of the card you'd like to look at. Enter -1 to decline the power."
+
+    def use_power(self, input):
+        #TODO: NEEDS MORE ERROR HANDLING
+        card = self.played_cards[-1]
+        if card.rank not in {'7', '8', '9', '10', 'J', 'Q', 'K'}:
+            return
+        if len(input) == 1 and input[0] == "-1":
+            return None, None
+        if card.rank in {'7', '8'}:
+            pos = int(input[0])
+            if self.players[self.turn].cards[pos] == None:
+                raise ValueError(f"You do not have a card in that position.")
+            return str(self.players[self.turn].cards[pos].name()), None
+        elif card.rank in {'9', '10' , 'K'}:
+            if len(input) < 2:
+                raise ValueError(f"Invalid input. Please try again.")
+            player = int(input[0])
+            pos = int(input[1])
+            if player > len(self.players) or player < 0:
+                raise ValueError(f"Player {player} does not exist.")
+            if player == self.turn:
+                raise ValueError(f"You cannot look at your own card, you must choose another player.")
+            if player == self.last_player and self.last_turn:
+                raise ValueError(f"You cannot use a power on the player who called Cambio.")
+            if self.players[player].cards[pos] == None:
+                raise ValueError(f"Player {player} does not have a card in that position.")
+            if card.rank != 'K':
+                return str(self.players[player].cards[pos].name()), None
+            return str(self.players[player].cards[pos].name()), True
+        elif card.rank in {'J', 'Q'}:
+            if len(input) < 3:
+                raise ValueError(f"Invalid input. Please try again.")
+            pos = int(input[0])
+            player = int(input[1])
+            pos2 = int(input[2])
+            if player == self.turn:
+                raise ValueError(f"You cannot swap with yourself, you must choose another player.")
+            if player == self.last_player and self.last_turn:
+                raise ValueError(f"You cannot use a power on the player who called Cambio.")
+            self.swap(pos, player, pos2)
+            return [pos, player, pos2], None
+
+    def look_at_two(self, player):
+        return [str(self.players[player].cards[0]), str(self.players[player].cards[1])]
+
+    def call_cambio(self):
+        self.last_turn = True
+        self.last_player = (self.turn + len(self.players)) % len(self.players)
+
+    def get_winner(self):
+        best_players = []
+        best_score = float('inf')
+        scores = []
+        for i, player in enumerate(self.players):
+            score = sum([card.get_value() for card in player.cards])
+            player.score += score
+            scores.append(score)
+            if score < best_score:
+                best_score = score
+                best_players = [i]
+            elif score == best_score:
+                best_players.append(i)
+        s = []
+        s.append("Scores for this game:")
+        for i, score in enumerate(scores):
+            s.append(f"\tPlayer {i}: {score}")
+        s.append("")
+
+        if len(best_players) == 1 and best_players[0] == self.last_player:
+            s.append(f"The winner is {best_players[0]}, the player who called Cambio.")
+        else:
+            s.append(f"Player {self.last_player} called Cambio and didn't win, so they will start with an additional card next round.")
+            self.extra_cards = self.last_player
+            if len(best_players) == 1:
+                s.append(f"The winner is Player {best_players[0]}.")
+            else:
+                s2 = f"The winners are: Player " + ", Player ".join(best_players) + "."
+                s.append(s2)
+
+        s.append("")
+
+        s.append("Total scores:")
+        for i, player in enumerate(self.players):
+            s.append(f"\tPlayer {i}: {player.score}")
+        
+        return "\n".join(s)
---
+++ server.py
@@ -0,0 +1,221 @@
+import json
+import select
+import socket
+from cambio import Cambio
+
+class CambioServer:
+    def __init__(self, num_players):
+        self.cambio = Cambio(num_players)
+        self.num_players = num_players
+        self.client_sockets = {} # Keys are sockets and values are addresses
+        #TODO: Make sure that only one client can connect from each address so that if a client crashes and reconnects we can look it up in this dict and see which player number it was
+        self.players = [] # Maps player number (index) to client socket
+        self.server_socket = None
+
+    def send_to_client(self, client_num, message):
+        print(message)
+
+    def send_to_all_clients(self, message):
+        print(message)
+
+    def accept_clients(self):
+        pass
+        #TODO: NEED TO WORK ON THIS
+        """
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind(('', self.port))
+        server_socket.listen(5)
+        self.port = server_socket.getsockname()[1]
+        print(f"Listening on port {self.port}")
+        #self.send_heartbeat()
+        
+        self.client_sockets[server_socket] = "server"
+        self.server_socket = server_socket
+
+        for _ in range(self.num_players):
+            (client_socket, address) = server_socket.accept()
+            print(f"Client {address} connected")
+            client_socket.settimeout(1)
+            self.client_sockets[client_socket] = address
+            self.players.append(client_socket)
+        
+        """
+
+    def send_game_state(self):
+        self.send_to_all_clients(self.cambio.game_state())
+
+    def wait_for_sticking(self):
+        #TODO: WORK ON THIS
+        #Timeout after 5 seconds?
+        """
+        while True:
+            #if input
+            try:
+                self.cambio.stick(input_client, input['player'], input['pos'])
+                if input_client != input['player']:
+                    try:
+                        self.cambio.give()
+                    except ValueError as e:
+                        pass
+                break
+            except ValueError as e:
+                self.send_to_all_clients(e)
+        """
+        pass
+
+    def get_client_input(self, client_num):
+        #TESTING
+        return input()
+    #TODO: ACTUALLY DO THIS
+
+    def play_game(self):
+        #TODO: Figure out how to restore from log and checkpoint in case the server previously crashed
+
+        self.cambio = Cambio(self.num_players)
+
+        while True:
+            self.cambio.setup()
+
+            for i, player in enumerate(self.cambio.players):
+                self.send_to_client(i, f"You are Player {i}. Your first two cards are {self.cambio.look_at_two(i)}")
+
+            game_over = False
+            while not game_over:
+                for i, player in enumerate(self.cambio.players):
+                    self.cambio.turn = i
+                    if self.cambio.last_turn and self.cambio.last_player == i:
+                        game_over = True
+                        break
+                    self.send_game_state()
+                    self.send_to_all_clients(f"Player {i}'s turn.")
+                    while True:
+                        self.send_to_client(i, "What would you like to do?\n\t1. Draw a card.\n\t2. Call Cambio.")
+                        turn_type = int(self.get_client_input(i))
+                        if turn_type == 1:
+                            self.cambio.draw()
+                            while True:
+                                self.send_to_client(i, f"You drew the {player.hand.name()}. What would you like to do?\n\t1. Replace one of your cards with the new card.\n\t2. Play the new card.")
+                                input = self.get_client_input(i)
+                                if input == "1":
+                                    while True:
+                                        self.send_to_client(i, "Which position would you like to switch with?")
+                                        pos = int(self.get_client_input(i))
+                                        try:
+                                            self.cambio.place(pos)
+                                            break
+                                        except ValueError as e:
+                                            self.send_to_client(i, e)
+                                    self.send_to_all_clients(f"Player {i} placed the drawn card in position {pos} and played the {self.cambio.played_cards[-1].name()}.")
+                                    break
+                                elif input == "2":
+                                    self.cambio.play()
+                                    self.send_to_all_clients(f"Player {i} played the {self.cambio.played_cards[-1].name()}.")
+                                    power = self.cambio.has_power()
+                                    if power:
+                                        while True:
+                                            self.send_to_client(i, power)
+                                            power_input = self.get_client_input(i).split()
+                                            try:
+                                                res, K_power = self.cambio.use_power(power_input)
+                                                if K_power:
+                                                    while True:
+                                                        self.send_to_client(i, f"That card is the {res}. Would you like to swap? If yes, enter the position of one of your cards that you would like to swap. If no, enter -1.")
+                                                        K_power_input = int(self.get_client_input(i))
+                                                        if K_power_input != -1:
+                                                            try:
+                                                                self.cambio.swap(K_power_input, int(power_input[0]), int(power_input[1]))
+                                                                break
+                                                            except ValueError as e:
+                                                                self.send_to_client(i, e)
+                                                elif res and isinstance(res, str):
+                                                    self.send_to_client(i, f"That card is the {res}.")
+                                                elif res and isinstance(res, list):
+                                                    self.send_to_all_clients(f"Player {i} switched the card in position {res[0]} with Player {res[1]}'s card in position {res[2]}.")
+                                                else:
+                                                    self.send_to_client(i, "The power has been declined.")
+                                                break
+                                            except ValueError as e:
+                                                self.send_to_client(i, e)
+                                    break
+                                else:
+                                    print("Please pick 1 or 2.")
+                            break
+                        elif turn_type == 2:
+                            if not self.cambio.last_turn:
+                                self.cambio.call_cambio()
+                                self.send_to_all_clients(f"Player {i} has called Cambio. This will be the last round of the game.")
+                                break
+                            else:
+                                self.send_to_client(i, "Cambio has already been called.")
+                        else:
+                            pass
+
+                    self.send_game_state()
+                    self.send_to_all_clients("All players may now attempt to stick a card.")
+                    self.wait_for_sticking()
+
+            self.send_to_all_clients(f"The game is over.\n\n{self.cambio.get_winner()}\nEnter 1 to play again or 0 to quit.")
+            #TODO: WAIT FOR INPUT AND IF ANY CLIENTS PRESS 1 CONTINUE
+            #IF ANY CLIENT PRESSES 0, RETURN BACK TO LOOP()       
+                    
+    def loop(self):
+        while True:
+            self.accept_clients()
+            self.play_game()
+    
+
+if __name__ == "__main__":
+    server = CambioServer(2)
+    server.loop()
+
+
+
+
+
+#First Attempt at client socket connections and stuff
+"""
+while True:
+    readable_sockets, _, _ = select.select(self.client_sockets.keys(), [], [], 1)
+
+    for sock in readable_sockets:
+        if sock == self.server_socket:
+            #Reconnect a client
+            try:
+                (client_socket, address) = sock.accept()
+                # Check to see if this is a player who had already connected
+                if address[0] in self.client_sockets.values():
+                    # Delete the old entry for the client that crashed
+                    for client in self.client_sockets:
+                        if self.client_sockets[client][0] == address[0]:
+                            del self.client_sockets[client]
+                            break
+                    print(f"Client {address} connected")
+                    client_socket.settimeout(1)
+                    self.client_sockets[client_socket] = address
+                    #TODO: REPLACE THE OLD SOCKET IN SELF.PLAYERS
+            except Exception as e:
+                print(f"Error accepting connection: {e}")
+        else:
+            try:
+                data = client_socket.recv(1024).decode('utf-8')
+                if not data:
+                    #TODO: REDO this
+                    print(f"Client {address} disconnected")
+                    client_socket.close()
+                    del self.client_sockets[client_socket]
+                    return
+                
+                request = json.loads(data)
+                action = request.get('action')
+
+                if sock == self.players[self.Cambio.turn]:
+                # Player whose turn it is
+                    pass
+                else:
+                    # CHECK HERE TO SEE IF ITS A STICK OTHERWISE DO NOTHING
+                    if action == "stick":
+                        pass
+            
+            except Exception as e:
+                print(f"Error: {e}")
+"""
